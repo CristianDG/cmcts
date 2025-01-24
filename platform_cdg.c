d86ed7df42344e332ebe9eba3bfd025b0177af32
CristianDG <39601699+CristianDG@users.noreply.github.com>
2025-01-24 14:49:58 -0300

    reimplementando dynamic array

--- platform_cdg.c
+++ platform_cdg.c
@@ -122,83 +122,49 @@ void arena_release(Arena arena) {
 #define CDG_CONTAINER_C
 // TODO: include arena.c
 
-#define dynamic_array_make(arena, dyn, cap) \
-  _dynamic_array_make(arena, (void **)dyn, cap, sizeof(*dyn))
-
-#define dynamic_array_push(dyn, item, arena) \
-  _dynamic_array_push((void **)dyn, arena, &item, sizeof(**dyn)) /* NOLINT */
-
-// NOTE: implementation from https://www.youtube.com/watch?v=_KSKH8C9Gf0 and https://nullprogram.com/blog/2023/10/05/
-typedef struct {
+typedef struct dynamic_array_header {
   u32 len;
   u32 cap;
-  u32 item_size;
-  u8  magic[4];
 } Dynamic_Array_Header;
 
-
-Dynamic_Array_Header *dynamic_array_header(void *arr) {
-  assert(arr != 0);
-
-  Dynamic_Array_Header *res = arr - sizeof(Dynamic_Array_Header);
-  assert(res->magic[0] == 'D');
-  assert(res->magic[1] == 'Y');
-  assert(res->magic[2] == 'N');
-  assert(res->magic[3] == 0);
-  return res;
+#define Dynamic_Array(type) \
+struct { \
+  u32 len; \
+  u32 cap; \
+  type *data; \
 }
 
-void dynamic_array_grow(void **arr, Arena *a){
-  Dynamic_Array_Header *header = dynamic_array_header(*arr);
-  assert(header->len >= header->cap);
+typedef Dynamic_Array(void) Any_Dynamic_Array;
 
-  u32 next_cap = header->cap * 2;
-  u32 next_size = header->item_size * next_cap;
+void dynamic_array_grow(Any_Dynamic_Array *arr, Arena *a, u32 item_size) {
+  Any_Dynamic_Array replica = {};
+  memcpy(&replica, arr, sizeof(replica));
 
-  void *new_place = arena_alloc(a, sizeof(Dynamic_Array_Header) + next_size);
-  memcpy(new_place, header, sizeof(Dynamic_Array_Header) + next_size);
-  header->cap = next_cap;
-
-  *arr = new_place + sizeof(Dynamic_Array_Header);
-}
-
-void dynamic_array_clear(void *arr){
-  Dynamic_Array_Header *header = dynamic_array_header(arr);
-  header->len = 0;
-}
-
-void _dynamic_array_make(Arena *a, void **arr, u32 initial_capacity, u32 item_size){
-  Dynamic_Array_Header *header = arena_alloc(a, sizeof(Dynamic_Array_Header) + (item_size * initial_capacity));
-
-  *header = (Dynamic_Array_Header) {
-    .item_size = item_size,
-    .cap = initial_capacity,
-    .len = 0,
-    .magic = "DYN",
-  };
-
-  *arr = header + 1;
-}
-
-u32 dynamic_array_len(void *arr) {
-  Dynamic_Array_Header *header = dynamic_array_header(arr);
-  return header->len;
-}
-
-void _dynamic_array_push(void **arr, Arena *a, void* item, u32 item_size){
-  if (*arr == 0) {
-    u32 initial_capacity = 32;
-    _dynamic_array_make(a, arr, initial_capacity, item_size);
+  replica.cap = replica.cap ? replica.cap : 1;
+  void *data = arena_alloc(a, 2 * item_size * replica.cap);
+  replica.cap *= 2;
+  if (replica.len) {
+    memcpy(data, replica.data, item_size*replica.len);
   }
-  Dynamic_Array_Header *header = dynamic_array_header(*arr);
-  if (header->len >= header->cap) {
-    dynamic_array_grow(arr, a);
-  }
-  assert(*arr != 0);
+  replica.data = data;
 
-  void *addr = ((void*)*arr) + (header->len++ * header->item_size);
-  memcpy(addr, item, item_size);
+  memcpy(arr, &replica, sizeof(replica));
 }
+
+#define dynamic_array_push(arr, item, arena) \
+  do { \
+    if ((arr)->len >= (arr)->cap) { \
+      dynamic_array_grow((Any_Dynamic_Array*)arr, arena, sizeof(*(arr)->data)); /* NOLINT */\
+    }\
+    (arr)->data[(arr)->len++] = item; \
+  } while (0)
+
+void _dynamic_array_clear(Any_Dynamic_Array *arr) {
+  arr->len = 0;
+}
+
+#define dynamic_array_clear(arr) _dynamic_array_clear((Any_Dynamic_Array *) arr)
+
 
 
 #endif
