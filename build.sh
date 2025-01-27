d02d4b1c405667ee4fe9eee3eda1639fb3be3912
CristianDG <professional.cristiandg@gmail.com>
2025-01-27 02:53:01 -0300

    melhorando build no windows

--- build.sh
+++ build.sh
@@ -2,11 +2,18 @@
 
 set -euo pipefail
 
+ON_WINDOWS=1
 flags=""
-flags+=" -lm"
-flags+=" -L raylib/lib"
-flags+=" -I raylib/include"
+flags+=" -L ./raylib/lib"
+flags+=" -I ./raylib/include"
 flags+=" -l raylib"
 flags+=" -g"
 
-clang -o mcts.bin platform_raylib.c $flags
+if [[ $ON_WINDOWS == 1 ]]; then
+  flags+=" -lmsvcrt -lOpenGL32 -lGdi32 -lWinMM -lkernel32 -lshell32 -lUser32 -Xlinker /NODEFAULTLIB:libcmt"
+else  
+  flags+=" -lm"
+fi
+
+
+clang -o mcts.exe platform_raylib.c $flags 
